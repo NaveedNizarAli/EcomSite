263bc1de07eca85c9e5afc97fff9b6ad24c4815c
naveednizar <naveednizar9@gmail.com>
2020-06-06 15:55:19 +0500

    Updates

---
+++ precache-manifest.497c9cdd17b48588a5b64e6864f4ace6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe013cd530c631245fe2a77b5db83907",
+    "url": "/EcomSite/index.html"
+  },
+  {
+    "revision": "77a7b910a44017b8d83b",
+    "url": "/EcomSite/static/css/main.6e9827c4.chunk.css"
+  },
+  {
+    "revision": "b76d3cedbca3ccdba849",
+    "url": "/EcomSite/static/js/2.b6766690.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/EcomSite/static/js/2.b6766690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a7b910a44017b8d83b",
+    "url": "/EcomSite/static/js/main.c5a1ed7f.chunk.js"
+  },
+  {
+    "revision": "f8c623b617f0636d2dce",
+    "url": "/EcomSite/static/js/runtime-main.50ac77de.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/EcomSite/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "db9eba89a7d6feda95ea1d508c893211",
+    "url": "/EcomSite/static/media/mens.db9eba89.jpg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/EcomSite/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "be91b57502ca0b4c9e2d62d95543a6d3",
+    "url": "/EcomSite/static/media/sneakers.be91b575.jpg"
+  },
+  {
+    "revision": "d7c263d256fedcee02718e2f558dce70",
+    "url": "/EcomSite/static/media/women.d7c263d2.jpg"
+  }
+]);
